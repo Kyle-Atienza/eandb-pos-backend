b9cc185b1642389c12439134de4acb6ec9c0a292
Kyle-Atienza <atienzakylefrancis@gmail.com>
2023-10-21 18:29:49 +0800

    updates from frontend

--- controllers/reportController.js
+++ controllers/reportController.js
@@ -5,6 +5,26 @@ const { mailTransporter } = require("../helpers/mailHelper");
 
 const sender = process.env.NODEMAILER_USER;
 
+const invoiceMatchByDateRange = (date_min, date_max) => {
+  const startOfDay = new Date(new Date().setUTCHours(0, 0, 0, 0));
+
+  const aggregation = {
+    $match: {
+      createdAt: {
+        $gte: startOfDay,
+      },
+    },
+  };
+
+  if (date_min) {
+    aggregation.$match.createdAt.$gte = new Date(date_min);
+  }
+  if (date_max) {
+    aggregation.$match.createdAt.$lte = new Date(date_max);
+  }
+
+  return aggregation;
+};
 const invoiceAggregation = [
   { $unwind: "$items" },
   {
@@ -142,6 +162,13 @@ const invoiceWokrsheetColumns = [
     width: 20,
   },
 ];
+const getWorkbookDateTitle = () => {
+  return (
+    new Date().toLocaleDateString().replaceAll("/", "-") +
+    "_" +
+    new Date().toLocaleTimeString().replaceAll(" ", "_")
+  );
+};
 
 const createWorksheet = (name, workbook, rows) => {
   const testWorksheet = workbook.addWorksheet(name);
@@ -154,23 +181,38 @@ const createWorksheet = (name, workbook, rows) => {
 };
 
 const getInvoiceReport = asyncHandler(async (req, res) => {
-  const { recipient } = req.query;
-  const invoiceReportPath = "./reports/invoice/test.xlsx";
+  const { recipient, date_min, date_max } = req.query;
+
+  const invoiceReportPath = `./reports/invoice/manual/${getWorkbookDateTitle()}.xlsx`;
 
   try {
+    let emailContent = "";
+    let invoices = [];
+
+    if (!date_min || !date_max) {
+      throw new Error("Dates are not provided");
+    }
+
     if (recipient) {
-      const invoices = await Invoice.aggregate(invoiceAggregation);
+      invoices = await Invoice.aggregate([
+        invoiceMatchByDateRange(date_min, date_max),
+        ...invoiceAggregation,
+      ]);
 
       const workbook = new Excel.Workbook();
       createWorksheet("Invoices", workbook, invoices);
       await workbook.xlsx.writeFile(invoiceReportPath);
       const buffer = await workbook.xlsx.writeBuffer();
 
-      await mailTransporter.sendMail({
+      emailContent = `Here is the invoice report from ${new Date(
+        date_min
+      ).toDateString()} to ${new Date(date_max).toDateString()}`;
+
+      mailTransporter.sendMail({
         from: sender,
         to: recipient,
-        subject: "Invoice Report",
-        text: "Here is the invoice report",
+        subject: "Manually Generated Invoice Report",
+        text: emailContent,
         attachments: [
           {
             filename: invoiceReportPath,
@@ -187,184 +229,6 @@ const getInvoiceReport = asyncHandler(async (req, res) => {
     res.status(500).json({ error });
   }
 });
-
-/* const getInvoiceReport = asyncHandler(async (req, res) => {
-  console.log("this");
-  const { recipient } = req.query;
-
-  try {
-    if (recipient) {
-      const invoices = await Invoice.aggregate([
-        { $unwind: "$items" },
-        {
-          $replaceRoot: {
-            newRoot: "$items",
-          },
-        },
-        {
-          $group: {
-            _id: "$item",
-            count: { $count: {} },
-          },
-        },
-        {
-          $project: {
-            productId: {
-              $let: {
-                vars: {
-                  id: {
-                    $split: ["$_id", "_"],
-                  },
-                },
-                in: { $toObjectId: { $arrayElemAt: ["$$id", 0] } },
-              },
-            },
-            variantId: {
-              $let: {
-                vars: {
-                  id: {
-                    $split: ["$_id", "_"],
-                  },
-                },
-                in: { $toObjectId: { $arrayElemAt: ["$$id", 1] } },
-              },
-            },
-            modifier: {
-              $let: {
-                vars: {
-                  id: {
-                    $split: ["$_id", "_"],
-                  },
-                },
-                in: { $arrayElemAt: ["$$id", 2] },
-              },
-            },
-            count: "$count",
-          },
-        },
-        {
-          $lookup: {
-            from: "products",
-            localField: "productId",
-            foreignField: "_id",
-            as: "product",
-          },
-        },
-        {
-          $unwind: "$product",
-        },
-        {
-          $addFields: {
-            variant: {
-              $let: {
-                vars: {
-                  index: {
-                    $indexOfArray: ["$product.variants._id", "$variantId"],
-                  },
-                },
-                in: {
-                  $arrayElemAt: ["$product.variants", "$$index"],
-                },
-              },
-            },
-            sales: "$variant.amount",
-          },
-        },
-        {
-          $set: {
-            sold: "$count",
-            variant: "$variant.name",
-            product: "$product.name",
-            sales: {
-              $multiply: ["$variant.amount", "$count"],
-            },
-          },
-        },
-        {
-          $fill: {
-            output: {
-              modifier: {
-                value: "",
-              },
-            },
-          },
-        },
-        {
-          $unset: [
-            "count",
-            "productId",
-            "variantId",
-            "product.variants",
-            "product.modifier",
-          ],
-        },
-        {
-          $sort: {
-            product: -1,
-          },
-        },
-      ]);
-
-      const workbook = new Excel.Workbook();
-      const testWorksheet = workbook.addWorksheet("Test Worksheet");
-      const columnWidth = 30;
-      testWorksheet.columns = [
-        {
-          header: "Product",
-          key: "product",
-          width: columnWidth,
-        },
-        {
-          header: "Variant",
-          key: "variant",
-          width: columnWidth,
-        },
-        {
-          header: "Modifier",
-          key: "modifier",
-          width: columnWidth,
-        },
-        {
-          header: "Sales",
-          key: "sales",
-          width: columnWidth,
-        },
-        {
-          header: "Sold",
-          key: "sold",
-          width: columnWidth,
-        },
-      ];
-
-      invoices.forEach((invoice, index) => {
-        const row = testWorksheet.getRow(index + 2);
-        row.values = invoice;
-      });
-
-      await workbook.xlsx.writeFile("./test.xlsx");
-      const buffer = await workbook.xlsx.writeBuffer();
-
-      await mailTransporter.sendMail({
-        from: "eandbpos@gmail.com", // sender address
-        to: recipient, // list of receivers
-        subject: "Subject",
-        text: "Report Send Test",
-        attachments: [
-          {
-            filename: "./test.xlsx",
-            content: buffer,
-          },
-        ],
-      });
-    } else {
-      throw new Error("Email recipient is not provided");
-    }
-    res.json({ message: "Your Report has been sent to " + recipient });
-  } catch (error) {
-    console.log(error);
-    res.status(500).json({ error });
-  }
-}); */
 
 module.exports = {
   getInvoiceReport,
